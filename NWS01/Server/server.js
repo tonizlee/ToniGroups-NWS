c2e0b6aea19258684905d1a0e1328e4e68cebc03
tonizlee <ezCareToni@gmail.com>
2018-07-23 11:27:37 +0900

    20180723

---
+++ NWS01/Server/server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express();
+var ejs = require('ejs')
+
+app.set('views', __dirname+'/views')
+app.set('view engine', 'html')
+app.engine('html',ejs.renderFile)
+
+app.get('/', function (req, res){
+    res.send('Test 4321')
+})
+
+app.get('/main', function (req, res){
+    res.render('main.html')
+})
+
+app.listen(4321, function(){
+    console.log("Connected 4321 Port")
+})
